8d84e3df303a1be13ddda376d63e2f9acd23550b
Kirilloao <kirilltaraturin747@gmail.com>
2023-10-11 13:20:17 +0200

    create calculatorView

--- Calculator/Controllers/MainViewController.swift
+++ Calculator/Controllers/MainViewController.swift
@@ -6,19 +6,25 @@
 //
 
 import UIKit
+import SnapKit
 
 final class MainViewController: UIViewController {
-
+    
+    // MARK: - Private UI Properties
+    private let calculatorView = CalculatorView()
+    
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(calculatorView)
+        setupConstraints()
     }
     
     // MARK: - Private Methods
     private func setupConstraints() {
-        
+        calculatorView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
-
-
 }
 
---
+++ Calculator/Views/CalculatorView.swift
@@ -0,0 +1,22 @@
+//
+//  CalculatorView.swift
+//  Calculator
+//
+//  Created by Kirill Taraturin on 11.10.2023.
+//
+
+import UIKit
+
+final class CalculatorView: UIView {
+
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+}
