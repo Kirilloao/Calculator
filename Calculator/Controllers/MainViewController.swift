c845540bfd203e157f36e937dd27d975e33566c2
Kirilloao <kirilltaraturin747@gmail.com>
2023-10-11 13:17:08 +0200

    create window

---
+++ Calculator/Controllers/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Calculator
+//
+//  Created by Kirill Taraturin on 11.10.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    // MARK: - Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Private Methods
+    private func setupConstraints() {
+        
+    }
+
+
+}
+
